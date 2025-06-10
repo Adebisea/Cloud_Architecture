12b1c86b45803a84ec70e21cb325b09059477604
Adebisea <abdulgkhadijah@gmail.com>
2025-06-10 13:34:04 +0100

    vpc and subnets cidr_blocks

---
+++ vpc/variables.tf
@@ -0,0 +1,27 @@
+variable "vpc_cidr_block" {
+  type    = string
+  default = "15.20.0.0/16"
+}
+
+variable "pub1_subnet_cidr_block" {
+  type    = string
+  default = "15.20.25.0/24"
+}
+
+variable "pub2_subnet_cidr_block" {
+  type    = string
+  default = "15.20.30.0/24"
+}
+
+variable "prv1_subnet_cidr_block" {
+  type    = string
+  default = "15.20.35.0/24"
+}
+
+variable "prv2_subnet_cidr_block" {
+  type    = string
+  default = "15.20.40.0/24"
+}
+
+
+
