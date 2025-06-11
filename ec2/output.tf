c5ecd0d4dab2adf6e94d1c2b37ebb15effeaf7cb
Adebisea <abdulgkhadijah@gmail.com>
2025-06-12 00:21:28 +0100

    updated outputs

--- ec2/output.tf
+++ ec2/output.tf
@@ -1,8 +1,16 @@
 output "private_key" {
-  value = tls_private_key.example.private_key_pem
+  value = tls_private_key.tls_prv_key.private_key_pem
   sensitive = true 
 }
 
 output "aws_key_pair_id" {
-  value = aws_key_pair.example.key_pair_id
+  value = aws_key_pair.prv_key.key_pair_id
+}
+
+output "instance_id"  {
+  value = aws_instance.ec2_techn.id
+  }
+
+output "ec2_sg" {
+    value = aws_security_group.allow_traffic.id
 }
