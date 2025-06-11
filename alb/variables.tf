cf476adb0cf5274b8538cbc8496971c866596bff
Adebisea <abdulgkhadijah@gmail.com>
2025-06-12 00:15:04 +0100

    alb variables

---
+++ alb/variables.tf
@@ -0,0 +1,19 @@
+variable "environment" {
+    type = string
+}
+
+variable "vpc_id" {
+    type = string
+}
+
+variable "instance_id" {
+    type = string
+}
+
+variable "ec2_sg" {
+    type = string
+}
+
+variable "public_subnets_ids" {
+    type = list(string)
+}
