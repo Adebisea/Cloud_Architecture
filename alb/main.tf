a07cd749b18ed1abba7986d15b7bd6996f534745
Adebisea <abdulgkhadijah@gmail.com>
2025-06-11 12:25:49 +0100

    created ALB Listeners and Target group

--- alb/main.tf
+++ alb/main.tf
@@ -22,6 +22,29 @@ resource "aws_lb" "alb" {
   }
 }
 
+resource "aws_lb_listener" "lb-listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lb-tg.arn
+  }
+}
+
+resource "aws_lb_target_group" "lb-tg" {
+  name     = "techn-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "alb-tg-attachment" {
+  target_group_arn = aws_lb_target_group.lb-tg.arn
+  target_id        = var.instance_id
+  port             = 80
+}
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "techn_alb_log_acess"
@@ -50,16 +73,16 @@ resource "aws_iam_policy" "policy" {
                                     "Effect": "Allow",
                                     "Principal": {
                                         "Service": "logdelivery.elasticloadbalancing.amazonaws.com"
-                                    },
+                                        },
                                     "Action": "s3:PutObject",
                                     "Resource": aws_s3_bucket.bucket.arn,
                                     "Condition": {
                                         "ArnLike": {
-                                        "aws:SourceArn": aws_lb.alb.arn
+                                            "aws:SourceArn": aws_lb.alb.arn
                                         }
                                     }
-                                    }
-                                ]
-                            }
-                        )
-                    } 
+                                }
+                            ]
+                        }
+                    )
+                } 
