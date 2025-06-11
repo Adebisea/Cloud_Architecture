c51e758695eac8703b6380081cbaeb30f9bf018f
Adebisea <abdulgkhadijah@gmail.com>
2025-06-12 00:20:06 +0100

    created ssm role for ec2 instance profile

--- ec2/main.tf
+++ ec2/main.tf
@@ -3,10 +3,6 @@
 resource "tls_private_key" "tls_prv_key" {
   algorithm = var.algorithm
   rsa_bits  = var.rsa_bits
-
-  tags = {
-    Name = "techn_tls_key"
-  }
 }
 
 resource "aws_key_pair" "prv_key" {
@@ -27,19 +23,6 @@ resource "aws_security_group" "allow_traffic" {
     cidr_blocks      = [var.ssh_cidr_block]
   }
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    security_groups = [var.alb_sg]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    security_groups = [var.alb_sg]
-  }
   egress {
     from_port   = 0
     to_port     = 0
@@ -65,6 +48,35 @@ data "aws_ami_ids" "ubuntu" {
 
 }
 
+resource "aws_iam_role" "ssm_ec2_role" {
+  name = "techn_ssm-ec2-role"
+
+  assume_role_policy = jsonencode(
+                                    {
+                                      Version = "2012-10-17",
+                                      Statement = [
+                                        {
+                                          Effect = "Allow",
+                                          Principal = {
+                                            Service = "ec2.amazonaws.com"
+                                          },
+                                          Action = "sts:AssumeRole"
+                                        }
+                                      ]
+                                    }
+                                  )
+                                }
+
+resource "aws_iam_role_policy_attachment" "ssm_ec2_policy" {
+  role       = aws_iam_role.ssm_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm_ec2_profile" {
+  name = "ssm-ec2-profile"
+  role = aws_iam_role.ssm_ec2_role.name
+}
+
 
 resource "aws_instance" "ec2_techn" {
   ami                    = data.aws_ami_ids.ubuntu.ids[0]
@@ -72,6 +84,7 @@ resource "aws_instance" "ec2_techn" {
   key_name               =  aws_key_pair.prv_key.key_name
   vpc_security_group_ids = [aws_security_group.allow_traffic.id]
   subnet_id              = var.prv2_subnet_id
+  iam_instance_profile  = aws_iam_instance_profile.ssm_ec2_profile.name
   root_block_device {
             volume_size  = 8
              }
@@ -80,4 +93,6 @@ resource "aws_instance" "ec2_techn" {
     Name = "ec2_techn"
     Environment = var.environment
   }
+
+  user_data = file("ec2/user_data.sh")
 }
