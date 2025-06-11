164b02f33c4e8ce3f172818b5c45f89813d9e13b
Adebisea <abdulgkhadijah@gmail.com>
2025-06-11 11:41:46 +0100

    added s3 bucket policy to allow alb access

--- alb/main.tf
+++ alb/main.tf
@@ -1,13 +1,5 @@
 #aws ALB resources
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = "techn_alb_log_acess"
-
-  tags = {
-    Name        = "techn_bucket"
-    Environment = var.environment
-  }
-}
 
 resource "aws_lb" "alb" {
   name               = "techn-alb"
@@ -29,3 +21,45 @@ resource "aws_lb" "alb" {
     Environment = "production"
   }
 }
+
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "techn_alb_log_acess"
+
+  tags = {
+    Name        = "techn_bucket"
+    Environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_policy" "bk_policy" {
+  bucket =aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "test_policy"
+  path        = "/"
+  description = "techn bucket policy to allow alb put objects in s3"
+  policy      = jsonencode(
+                            {
+                                "Version": "2012-10-17",
+                                "Statement": [
+                                    {
+                                    "Sid": "AllowELBLogDelivery",
+                                    "Effect": "Allow",
+                                    "Principal": {
+                                        "Service": "logdelivery.elasticloadbalancing.amazonaws.com"
+                                    },
+                                    "Action": "s3:PutObject",
+                                    "Resource": aws_s3_bucket.bucket.arn,
+                                    "Condition": {
+                                        "ArnLike": {
+                                        "aws:SourceArn": aws_lb.alb.arn
+                                        }
+                                    }
+                                    }
+                                ]
+                            }
+                        )
+                    } 
