6a14f3d61228173829bc3ecd3868edc531db7a3e
Adebisea <abdulgkhadijah@gmail.com>
2025-06-11 10:24:51 +0100

    vpc outpouts

---
+++ vpc/output.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = aws_vpc.vpc.id
+}
+
+output "prv1_subnet_id" {
+  value = aws_subnet.prv1_subnet.id
+}
+
+output "prv2_subnet_id" {
+  value = aws_subnet.prv2_subnet.id
+}
+
+
+
+
