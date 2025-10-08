a89df5b8e55890acfd0b8bc80f2f4e9904b12047
Adebisea <abdulgkhadijah@gmail.com>
2025-10-08 15:14:04 +0100

    comment out policy attachment

--- Infra/ec2/main.tf
+++ Infra/ec2/main.tf
@@ -90,10 +90,10 @@ resource "aws_iam_role_policy_attachment" "ssm_ec2_policy" {
 #   })
 # }
 
-resource "aws_iam_role_policy_attachment" "attach_secrets_policy" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = aws_iam_policy.secrets_access.arn
-}
+# resource "aws_iam_role_policy_attachment" "attach_secrets_policy" {
+#   role       = aws_iam_role.ec2_role.name
+#   policy_arn = aws_iam_policy.secrets_access.arn
+# }
 
 # iam_instance_profile
 resource "aws_iam_instance_profile" "ec2_profile" {
