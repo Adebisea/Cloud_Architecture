7e903e7f6b1e279d1e9e09111e4a5ac72c0ff633
Adebisea <abdulgkhadijah@gmail.com>
2025-12-31 13:00:11 +0100

    fix ec2-s3-policy name error

--- Infra/ec2/main.tf
+++ Infra/ec2/main.tf
@@ -133,7 +133,7 @@ resource "aws_iam_role_policy" "ec2_s3_policy" {
     ]
   })
 }
-resource "aws_iam_role_policy_attachment" "attach_secrets_policy" {
+resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = aws_iam_policy.ec2_s3_policy.arn
 }
