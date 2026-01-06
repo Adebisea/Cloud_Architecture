cc449c821efb58b70ec7f600de8ba65c7049b9dc
Adebisea <abdulgkhadijah@gmail.com>
2026-01-06 12:14:35 +0100

    remove keypair outputs

--- Infra/ec2/output.tf
+++ Infra/ec2/output.tf
@@ -1,11 +1,3 @@
-output "private_key" {
-  value = tls_private_key.tls_prv_key.private_key_pem
-  sensitive = true 
-}
-
-output "aws_key_pair_id" {
-  value = aws_key_pair.prv_key.key_pair_id
-}
 
 output "instance_id"  {
   value = aws_instance.ec2_techn.id
